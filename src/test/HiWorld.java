14c816a4a6fc3d1bf9b7f6e7ab04084c7787a222
lewismoriarty <1600107997@qq.com>
2020-05-11 14:26:47 +0800

    test

--- src/test/HiWorld.java
+++ src/test/HiWorld.java
@@ -3,6 +3,6 @@ package test;
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
-        System.out.println("github is good");
+
     }
 }
